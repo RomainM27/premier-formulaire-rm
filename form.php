9e02377d44f5ef2deae854ee0c09a5172480fc37
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-03-22 14:22:11 +0100

    validation in js done

---
+++ form.php
@@ -0,0 +1,73 @@
+<?php 
+    include 'contry.php';
+?>
+
+<form id="signup" class="form">
+    <div class="form-field">
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" autocomplete="off">
+        <small></small>
+    </div>
+    <div class="form-field">
+        <label for="lastname">Lastname:</label>
+        <input type="text" name="lastname" id="lastname" autocomplete="off">
+        <small></small>
+    </div>
+    <div class="form-field gender">
+        <p>Gender:</p>
+        <input type="radio" id="gender1"
+        name="gender" value="male">
+        <label for="gender1">Male</label>
+        <input type="radio" id="gender2"
+        name="gender" value="female">
+        <label for="gender2">Female</label>
+
+        <input type="radio" id="gender3"
+        name="gender" value="other">
+        <label for="gender3">Other</label>
+
+        <small></small>
+    </div>
+    <!-- gender -->
+
+    <div class="form-field">
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email" autocomplete="off">
+        <small></small>
+    </div>
+
+    <div class="form-field">
+        <label for="country">Country:</label>
+        <select name="country" id="country">
+            <option value="">--Please choose an option--</option>
+            <?php 
+            foreach ($country as $key => $value) {
+                echo '<option value="'.$key.'">'.$value.'</option>';
+            }
+            ?>
+        </select>
+        <small></small>
+    </div>
+
+
+    <div class="form-field">
+        <label for="subject">Subject:</label>
+        <select name="subject" id="subject">
+            <option value="">--Please choose an option--</option>
+            <option value="question">Question ?</option>
+            <option value="problem">Problem ?</option>
+            <option value="other">Other ?</option>
+        </select>
+        <small></small>
+    </div>
+
+    <div class="form-field">
+        <label for="textarea">Your message:</label>
+        <textarea name="textarea" id="textarea" rows="5">
+        </textarea>
+        <small></small>
+    </div>
+    <div class="form-field">
+        <input type="submit" value="Sign Up" class="btn">
+    </div>
+</form>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>form</title>
+</head>
+<body>
+
+    <section class="formulaire">
+        <div id="formulaire-ensemble">
+            <div class="header-form">
+            <img src="./assets/img/hackers-poulette-logo.png" alt="hackers-poulette-logo">
+            <h1> Contact US</h1>
+            </div>
+            <div class="body-form">
+                <?php include 'form.php' ?>
+            </div>
+        
+        </div>
+    
+    </section>
+    <script src="./assets/js/index.js"></script>
+</body>
+</html>
